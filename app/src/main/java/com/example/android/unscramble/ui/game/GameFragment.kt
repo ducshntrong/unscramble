1fa499f94253e700290e4255983845472af8eb2e
ducshntrong <huynhtrongduc01225445811@gmail.com>
2023-03-09 10:10:49 +0700

    unscramble

--- app/src/main/java/com/example/android/unscramble/ui/game/GameFragment.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameFragment.kt
@@ -9,6 +9,7 @@ import androidx.fragment.app.viewModels
 import com.example.android.unscramble.R
 import com.example.android.unscramble.databinding.GameFragmentBinding
 import android.util.Log
+import androidx.databinding.DataBindingUtil
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 /**
@@ -37,10 +38,14 @@ class GameFragment : Fragment() {
             savedInstanceState: Bundle?
     ): View {
         // Inflate  tệp XML bố cục và trả về một thể hiện đối tượng liên kết
-        binding = GameFragmentBinding.inflate(inflater, container, false)
+        //thay đổi cách tạo bản sao của biến binding để sử dụng tính năng liên kết dữ liệu.
+        binding = DataBindingUtil.inflate(inflater, R.layout.game_fragment, container, false)//2.7
         //Lệnh gọi lại onCreateView() sẽ được kích hoạt khi mảnh được tạo lần đầu tiên và cũng được
         //kích hoạt mỗi khi được tạo lại cho mọi sự kiện (chẳng hạn như thay đổi cấu hình).
-        Log.d("GameFragment", "GameFragment created/re-created!")//6
+//        Log.d("GameFragment", "GameFragment created/re-created!")//6
+        //in dữ liệu ứng dụng, từ, điểm và số từ.
+        Log.d("GameFragment", "Word: ${viewModel.currentScrambledWord} " +
+                "Score: ${viewModel.score} WordCount: ${viewModel.currentWordCount}")//11
         return binding.root
     }
 
@@ -51,7 +56,7 @@ class GameFragment : Fragment() {
             //đặt tiêu đề trên hộp thoại thông báo, sử dụng tài nguyên chuỗi qua strings.xml
             .setTitle(getString(R.string.congratulations))
             //Thiết lập thông báo để hiện điểm cuối cùng, sử dụng phiên bản chỉ có thể đọc của biến điểm (viewModel.score )
-            .setMessage(getString(R.string.you_scored, viewModel.score))
+            .setMessage(getString(R.string.you_scored, viewModel.score.value))
             //Đảm bảo không huỷ được hộp thoại thông báo khi người dùng nhấn phím quay lại
             .setCancelable(false)
             //Thêm hai nút văn bản EXIT (THOÁT) và PLAY AGAIN (CHƠI LẠI) bằng cách sử dụng các phương thức
@@ -74,15 +79,43 @@ class GameFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        //khởi chạy các biến bố cục gameViewModel và maxNoOfWords
+        binding.gameViewModel = viewModel//2.8
+        binding.maxNoOfWords = MAX_NO_OF_WORDS//2.8
+
+        //LiveData có thể quan sát nhận biết được vòng đời nên bạn phải chuyển chủ sở hữu vòng đời vào bố cục
+        // Chỉ định chế độ xem phân đoạn là chủ sở hữu vòng đời của liên kết.
+        // Điều này được sử dụng để ràng buộc có thể quan sát các cập nhật LiveData
+        binding.lifecycleOwner = viewLifecycleOwner//2.8
+
+        //2.5****Đính kèm đối tượng tiếp nhận dữ liệu vào đối tượng LiveData****
+        // Quan sát ScrambledCharArray LiveData, truyền vào LifecycleOwner và the observer.
+//        viewModel.currentScrambledWord.observe(viewLifecycleOwner,//2.5
+//            //Trong nội dung hàm của biểu thức lambda, gán newWord cho chế độ xem văn bản từ bị xáo trộn.
+//            { newWord -> binding.textViewUnscrambledWord.text = newWord
+//            }) // xoá mã đối tượng tiếp nhận dữ liệu LiveData cho currentScrambledWord/2.9
+                //Bố cục sẽ trực tiếp nhận được thông tin cập nhật về những thay đổi đối với LiveData
+        //vào lúc này chế độ xem văn bản từ bị xáo trộn sử dụng biểu thức liên kết để cập nhật giao diện 2.9
+        // người dùng chứ không phải LiveData đối tượng tiếp nhận dữ liệu.
 
         // Thiết lập trình nghe nhấp chuột cho các nút submit và skip.
         binding.submit.setOnClickListener { onSubmitWord() }
         binding.skip.setOnClickListener { onSkipWord() }
         // Update the UI
-        updateNextWordOnScreen()
-        binding.score.text = getString(R.string.score, 0)
-        binding.wordCount.text = getString(
-                R.string.word_count, 0, MAX_NO_OF_WORDS)
+//        updateNextWordOnScreen()
+
+//        //đính kèm đối tượng tiếp nhận dữ liệu cho score
+//        viewModel.score.observe(viewLifecycleOwner,//2.6
+//            //trong biểu thức lambda, chuyển điểm mới làm tham số và bên trong nội dung hàm,
+//            // đặt điểm mới thành chế độ xem văn bản.
+//            { newScore -> binding.score.text = getString(R.string.score, newScore)
+//            })
+//        //đính kèm đối tượng tiếp nhận dữ liệu cho currentWordCount
+//        viewModel.currentWordCount.observe(viewLifecycleOwner,//2.6
+//            { newWordCount ->
+//                // đặt số từ mới cùng với MAX_NO_OF_WORDS thành chế độ xem văn bản.
+//                binding.wordCount.text = getString(R.string.word_count, newWordCount, MAX_NO_OF_WORDS)
+//            })
     }
 
     /*
@@ -90,12 +123,17 @@ class GameFragment : Fragment() {
      * Hiển thị từ xáo trộn tiếp theo.
      */
     private fun onSubmitWord() {
-        currentScrambledWord = getNextScrambledWord()
-        currentWordCount++
-        score += SCORE_INCREASE
-        binding.wordCount.text = getString(R.string.word_count, currentWordCount, MAX_NO_OF_WORDS)
-        binding.score.text = getString(R.string.score, score)
-        setErrorTextField(false)
+        //Lưu trữ từ của người chơi trong đó, bằng cách trích xuất từ qua trường văn bản trong biến binding
+        val playerWord = binding.textInputEditText.text.toString()
+        //if để kiểm tra từ của người chơi bằng phương thức isUserWordCorrect(), truyền vào playerWord
+        if (viewModel.isUserWordCorrect(playerWord)){
+            setErrorTextField(false)
+            if (!viewModel.nextWord()) {
+               showFinalScoreDialog()
+            }
+        }else{
+            setErrorTextField(true)
+        }
     }
 
     /*
@@ -103,10 +141,14 @@ class GameFragment : Fragment() {
       * Tăng số lượng từ.
       */
     private fun onSkipWord() {
-        currentScrambledWord = getNextScrambledWord()
-        currentWordCount++
-        binding.wordCount.text = getString(R.string.word_count, currentWordCount, MAX_NO_OF_WORDS)
-        setErrorTextField(false)
+        //Nếu true, hãy hiện từ đó trên màn hình và thiết lập lại trường văn bản.
+        // Nếu false và không còn từ nào ở vòng này, hãy hiện hộp thoại thông báo có điểm cuối cùng.
+        if (viewModel.nextWord()){
+            setErrorTextField(false)
+//            updateNextWordOnScreen()
+        }else {
+            showFinalScoreDialog()
+        }
     }
 
     /*
@@ -123,8 +165,9 @@ class GameFragment : Fragment() {
       * khởi động lại trò chơi.
       */
     private fun restartGame() {
+        viewModel.reinitializeData()
         setErrorTextField(false)
-        updateNextWordOnScreen()
+//        updateNextWordOnScreen()
     }
 
     /*
@@ -136,6 +179,9 @@ class GameFragment : Fragment() {
 
     /*
      * Đặt và đặt lại trạng thái lỗi trường văn bản.
+     * được xác định để giúp bạn thiết lập và thiết lập lại lỗi trong trường văn bản.
+     * Hãy gọi phương thức này bằng true hoặc false dưới dạng tham số đầu vào, tuỳ theo bạn
+     * có muốn hiện lỗi trong trường văn bản hay không.
      */
     private fun setErrorTextField(error: Boolean) {
         if (error) {
@@ -152,7 +198,7 @@ class GameFragment : Fragment() {
       * phương thức updateNextWordOnScreen() để sử dụng thuộc tính viewModel chỉ có thể đọc là currentScrambledWord
       * Di chuyển dữ liệu sang ViewModel
       */
-    private fun updateNextWordOnScreen() {
-        binding.textViewUnscrambledWord.text = viewModel.currentScrambledWord
-    }
+//    private fun updateNextWordOnScreen() {
+//        binding.textViewUnscrambledWord.text = viewModel.currentScrambledWord
+//    }
 }
--- app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -1,7 +1,13 @@
 package com.example.android.unscramble.ui.game
 
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.TtsSpan
 import androidx.lifecycle.ViewModel
 import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 
 class GameViewModel : ViewModel() {//Thêm viewModel
     // ********7.ĐIỀN VIEWMODEL********
@@ -12,15 +18,50 @@ class GameViewModel : ViewModel() {//Thêm viewModel
 
     //****5.DI CHUYỂN DỮ LIỆU SANG ViewModel****
     //****8.HỘP THOẠI****
-    private var _score = 0//5,8. thêm thuộc tính sao lưu vào biến score
-    val score: Int
-        get() = _score
+//    private var _score = 0//5,8. thêm thuộc tính sao lưu vào biến score
+//    val score: Int
+//        get() = _score
+//    //11. *****Xác minh ViewModel lưu giữ dữ liệu****
+//    private val _currentWordCount = 0//5,11
+//    //Thêm trường sao lưu.
+//    val currentWordCount: Int//11
+//        get() = _currentWordCount
 
-    private var currentWordCount = 0//5
     //Thêm thuộc tính sao lưu vào currentScrambledWord
-    private lateinit var _currentScrambledWord: String//5
-    val currentScrambledWord: String //5
-        get() = _currentScrambledWord
+//    private lateinit var _currentScrambledWord: String//5
+//    val currentScrambledWord: String //5
+//        get() = _currentScrambledWord
+
+    //2.6****Đính kèm đối tượng tiếp nhận dữ liệu vào điểm số và số từ****
+    private val _score = MutableLiveData(0)//2.6
+    val score: LiveData<Int>
+        get() = _score
+    private val _currentWordCount = MutableLiveData(0)//2.6
+    val currentWordCount: LiveData<Int>
+        get() = _currentWordCount
+
+    //2.4 ****Thêm LiveData vào từ bị xáo trộn hiện tại****
+    //Thay đổi loại biến _currentScrambledWord thành val vì giá trị của đối tượng
+    // LiveData/MutableLiveData sẽ giữ nguyên và chỉ có dữ liệu được lưu trữ trong đối tượng mới thay đổi.
+    private val _currentScrambledWord = MutableLiveData<String>()//2.4
+    //Thay đổi trường sao lưu, thay đổi loại currentScrambledWord thành LiveData<String>, vì trường này không thể thay đổi
+//    val currentScrambledWord: LiveData<String>//2.4
+//        get() = _currentScrambledWord
+    val currentScrambledWord: LiveData<Spannable> = Transformations.map(_currentScrambledWord) {
+        if (it == null) {
+            SpannableString("")
+        } else {
+            val scrambledWord = it.toString()
+            val spannable: Spannable = SpannableString(scrambledWord)
+            spannable.setSpan(
+                TtsSpan.VerbatimBuilder(scrambledWord).build(),
+                0,
+                scrambledWord.length,
+                Spannable.SPAN_INCLUSIVE_INCLUSIVE
+            )
+            spannable
+        }
+    }
 
     //phương thức getNextWord() để lấy từ được xáo trộn tiếp theo
     private fun getNextWord() {//7
@@ -40,8 +81,10 @@ class GameViewModel : ViewModel() {//Thêm viewModel
         if (wordsList.contains(currentWord)){
             getNextWord()
         } else{
-            _currentScrambledWord = String(tempWord)
-            ++currentWordCount
+            //Để truy cập vào dữ liệu trong đối tượng LiveData, sử dụng thuộc tính value
+            _currentScrambledWord.value = String(tempWord)//2.4
+            //sử dụng hàm Kotlin inc() để giá trị tăng thêm một bằng giá trị rỗng an toàn
+            _currentWordCount.value = (_currentWordCount.value)?.inc()//2.6
             wordsList.add(currentWord)
         }
     }
@@ -55,5 +98,42 @@ class GameViewModel : ViewModel() {//Thêm viewModel
     override fun onCleared() {//6
         super.onCleared()
         Log.d("GameFragment", "GameViewModel destroyed!")
+    }
+    //9.*****nút submit*****
+    private fun increaseScore() {
+        //Tăng biến score bằng SCORE_INCREASE.
+//        _score += SCORE_INCREASE
+        //Sử dụng hàm Kotlin plus() để tăng giá trị _score. Giá trị này bổ sung giá trị rỗng an toàn.
+        _score.value = (_score.value)?.plus(SCORE_INCREASE)//2.6
+    }
+    //isUserWordCorrect() để trả về Boolean và lấy String (từ của người chơi) làm tham số.
+    fun isUserWordCorrect(playerWord: String): Boolean {//9
+        //xác thực từ của người chơi và cộng điểm nếu đoán đúng.
+        // Thao tác này sẽ cập nhật điểm cuối cùng trong hộp thoại thông báo.
+        if (playerWord.equals(currentWord, true)) {
+            increaseScore()
+            return true
+        }
+        return false
+    }
+
+    /*
+    * Trả về true nếu số từ hiện tại nhỏ hơn MAX_NO_OF_WORDS.
+    * Cập nhật từ tiếp theo.
+    */
+    fun nextWord(): Boolean {//7
+        return if (_currentWordCount.value!! < MAX_NO_OF_WORDS) {
+            getNextWord()
+            true
+        } else false
+    }
+
+    //Tái tạo dữ liệu trò chơi để khởi động lại trò chơi
+    fun reinitializeData(){
+        //Thiết lập điểm và số từ thành 0. Xoá danh sách từ và gọi phương thức getNextWord().
+        _score.value = 0
+        _currentWordCount.value = 0
+        wordsList.clear()
+        getNextWord()
     }
 }
